815883e3537bc5e3b6716180293bf1385ff93eb4
MinsuJeon <chso294@naver.com>
2022-07-07 17:24:18 +0900

    feat: BankManagerConsoleApp 모델 파일을 BankManagerUIApp으로 복사

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BankManagerConsoleApp - main.swift
-//  Created by 수꿍, 브래드. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
-var bankManagerConsole = BankManagerConsole()
-
-bankManagerConsole.run()
